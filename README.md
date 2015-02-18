db46e47782bca91b2bdcc61278e0a13ed394b991
BufferUnderrunOK <barrett.nuzum@gmail.com>
2015-02-17 22:51:33 -0600

    Updated Readme. Script completes step 5 now.

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 This is the course project for Getting & Cleaning Data for February 2015.
 See the Codebook for details on how the data exists, and is set up.
 
+This requires 'plyr' and 'dplyr' libraries.
+Please run 
+```
+install.packages('plyr')
+```
+and
+```
+install.packages('dplyr')
+```
+before executing.
+
 Barrett
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+library(plyr)
+library(dplyr)
+
 run_analysis <- function() {
         rm(list = ls())
 
@@ -17,21 +20,30 @@ run_analysis <- function() {
 
         X_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$feature_name, quote="\"")        
         y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c('activity_id'), quote="\"")
+        subjects_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = c('subject_id'), quote="\"")
 
+        # splice in the activity names and subject IDs
         targetDF <- X_test[,allNdx]
-        zhaan <- merge(y_test, activity_labels)
-        targetDF$activity_name <- zhaan$activity_name
+        joined <- join(y_test, activity_labels)
+        targetDF$activity_name <- joined$activity_name
+        targetDF$subject_id <- subjects_test$subject_id
 
         X_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$feature_name, quote="\"")
         y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = c('activity_id'), quote="\"")
+        subjects_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = c('subject_id'), quote="\"")
 
+        # splice in the activity names and subject IDs
         targetDF2 <- X_train[,allNdx]
-        zhaan <- merge(y_train, activity_labels)
-        targetDF2$activity_name <- zhaan$activity_name
+        joined <- join(y_train, activity_labels)
+        targetDF2$activity_name <- joined$activity_name
+        targetDF2$subject_id <- subjects_train$subject_id
 
-##        newrowmin <- nrow(targetDF) + 1
-##        newrowmax <- newrowmin+nrow(targetDF2) - 1
-##        rownames(targetDF2) <- newrowmin:newrowmax
-        
-        rbind(targetDF,targetDF2)
+        ## rbind combines the test and train data frames, which is the output for Step 4
+        step4 <- rbind(targetDF,targetDF2)
+
+        ## step 5: pivot the data using dplyr
+        ## dplyr is *amazingly* powerful
+        ## http://marcoghislanzoni.com/blog/2014/09/01/pivot-tables-r-dplyr/ for usage of the pipe %>% operator and group_by
+        ## http://stackoverflow.com/a/25839767 for summarise_each
+        step5 <- step4 %>% group_by(subject_id,activity_name) %>% summarise_each(funs(mean))
 }
